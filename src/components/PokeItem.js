306562573ee338bc3153f44d446df29d6a7b6e8e
sector032 <szantodavid032@gmail.com>
2020-12-18 15:59:26 +0100

    pictures added

--- src/components/PokeItem.js
+++ src/components/PokeItem.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
 
 export class PokeItem extends Component {
+  state = {
+    image : null
+  };
+  componentDidMount(){
+    axios.get(this.props.url).then((res) => this.setState({ image: res.data.sprites.front_default }));
+  }
+
   render() {
     return (
       <div>
+        <img src={this.state.image}></img>
         <p>{this.props.name}</p>
       </div>
     );
