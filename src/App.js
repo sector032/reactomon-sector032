5fd5b54d279252bcd48a1ab7ed73914b5a211471
sector032 <szantodavid032@gmail.com>
2021-01-04 11:09:08 +0100

    theme changer added

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import MainPage from './components/MainPage';
 import PokeDetail from './components/PokeDetail';
 import axios from 'axios';
 import './App.css';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	body{
+		background-color: ${(props) => (props.theme.mode === 'dark' ? '#111' : '#EEE')};
+		color: ${(props) => (props.theme.mode === 'dark' ? '#EEE' : '#111')};
+	}
+`;
 
 const App = (props) => {
 	const [pokemons, setPokemons] = useState([]);
 	const [types, setTypes] = useState([]);
+	const [theme, setTheme] = useState({ mode: 'dark' });
 
 	useEffect(() => {
 		axios
@@ -21,22 +30,34 @@ const App = (props) => {
 			.then((response) => setTypes(response.data.results));
 	}, []);
 	return (
-		<Router>
-			<div className='App'>
-				<div className='container'>
-					<Navbar />
-					<Route exact path='/' component={MainPage} />
-					<Route
-						path='/pokemons'
-						render={(props) => <PokeList pokemons={pokemons} />}
-					/>
-					<Route path='/types' render={(props) => <Types types={types} />} />
-					<Route path='/pokemon/:id'>
-						<PokeDetail />
-					</Route>
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<Router>
+				<div className='App'>
+					<div className='container'>
+						<Navbar />
+						<button
+							onClick={(e) =>
+								setTheme(
+									theme.mode === 'dark' ? { mode: 'light' } : { mode: 'dark' }
+								)
+							}
+						>
+							ToggleTheme
+						</button>
+						<Route exact path='/' component={MainPage} />
+						<Route
+							path='/pokemons'
+							render={(props) => <PokeList pokemons={pokemons} />}
+						/>
+						<Route path='/types' render={(props) => <Types types={types} />} />
+						<Route path='/pokemon/:id'>
+							<PokeDetail />
+						</Route>
+					</div>
 				</div>
-			</div>
-		</Router>
+			</Router>
+		</ThemeProvider>
 	);
 };
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -14,17 +14,6 @@ export default function Navbar() {
 				<a class='navbar-brand' href='#'>
 					PokeBar :{' '}
 				</a>
-				<button
-					class='navbar-toggler'
-					type='button'
-					data-toggle='collapse'
-					data-target='#navbarNavDropdown'
-					aria-controls='navbarNavDropdown'
-					aria-expanded='false'
-					aria-label='Toggle navigation'
-				>
-					<span class='navbar-toggler-icon'></span>
-				</button>
 				<div class='collapse navbar-collapse' id='navbarNavDropdown'>
 					<ul class='navbar-nav'>
 						<li class='nav-item active'>
@@ -54,9 +43,4 @@ const headerStyle = {
 	padding: '10px',
 	background: 'red',
 	color: '#fff',
-};
-
-const navStyle = {
-	textAlign: 'center',
-	padding: '10px',
 };
